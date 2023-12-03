902410fcbbc574be4e9388033b40de417ac0cd25
DivyaP <divyapatidar2000@gmail.com>
2023-12-03 13:48:37 +0530

    Udemy -WebApi

---
+++ API/Program.cs
@@ -0,0 +1,44 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add Db Context Service 
+builder.Services.AddDbContext<StoreContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//alternative way of creating db
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try{
+    context.Database.Migrate();  //apply pending migrations and create db if doesn't exist
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex){
+    logger.LogError(ex, "A problem occurred during migration");
+}
+
+app.Run();
